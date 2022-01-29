1289e7b14be9f713aa55c87b9b66d8539c43c7dd
Eric Trombly <etrombly@yahoo.com>
2022-01-29 15:05:50 -0600

    make input dynamic

--- build.rs
+++ build.rs
@@ -1,10 +1,12 @@
-use syntect::parsing::SyntaxSetBuilder;
-use syntect::highlighting::ThemeSet;
 use syntect::dumps::dump_to_file;
+use syntect::highlighting::ThemeSet;
+use syntect::parsing::SyntaxSetBuilder;
 
 fn main() {
     let mut syntax_set_builder = SyntaxSetBuilder::new();
-    syntax_set_builder.add_from_folder("./assets", true).unwrap();
+    syntax_set_builder
+        .add_from_folder("./assets", true)
+        .unwrap();
     let syntax_set = syntax_set_builder.build();
     dump_to_file(&syntax_set, "./assets/ss.bin").unwrap();
     let ts = ThemeSet::load_from_folder("./assets").unwrap();
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use yew::prelude::*;
-use yew::virtual_dom::VNode;
-use web_sys::Node;
-use once_cell::sync::Lazy;
 use instant::Instant;
+use once_cell::sync::Lazy;
+use syntect::dumps::from_binary;
+use syntect::highlighting::ThemeSet;
 use syntect::html::highlighted_html_for_string;
 use syntect::parsing::SyntaxSet;
-use syntect::highlighting::ThemeSet;
-use syntect::dumps::from_binary;
+use yew::prelude::*;
+use yew::Html;
+use web_sys::Element;
+use web_sys::HtmlTextAreaElement;
+use wasm_bindgen::JsCast;
+use wasm_bindgen::UnwrapThrowExt;
 
 static PS: Lazy<SyntaxSet> = Lazy::new(|| {
     let ss_bytes = include_bytes!("../assets/ss.bin");
@@ -18,55 +21,55 @@ static TS: Lazy<ThemeSet> = Lazy::new(|| {
     from_binary(ts_bytes)
 });
 
-#[derive(Debug, Clone, Eq, PartialEq, Properties)]
-struct RawHTMLProps {
-    pub inner_html: String,
+pub enum Msg {
+    Update(InputEvent),
 }
 
-struct RawHTML;
+pub struct App {
+    div: Element,
+    time: String,
+}
 
-impl Component for RawHTML {
-    type Message = ();
-    type Properties = RawHTMLProps;
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        Self
+        let div = gloo_utils::document().create_element("div").unwrap();
+        Self {
+            div,
+            time: "0".into(),
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::Update(e) => {
+                let now = Instant::now();
+                let event: Event = e.dyn_into().unwrap_throw();
+                let event_target = event.target().unwrap_throw();
+                let target: HtmlTextAreaElement = event_target.dyn_into().unwrap_throw();
+                let value = target.value();
+                let syntax = PS.find_syntax_by_extension("rs").unwrap();
+                let theme = &TS.themes["base16-ocean.dark"];
+                let html = highlighted_html_for_string(&value, &PS, syntax, theme);
+                self.div.set_inner_html(&html);
+                self.time = format!("Highlight time: {}", now.elapsed().as_millis());
+                true
+            }
+        }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        let div = web_sys::window()
-            .unwrap()
-            .document()
-            .unwrap()
-            .create_element("div")
-            .unwrap();
-        div.set_inner_html(&ctx.props().inner_html[..]);
-
-        let node = Node::from(div);
-        let vnode = VNode::VRef(node);
-        vnode
-    }
-}
-
-#[function_component(App)]
-fn app() -> Html {
-    let now = Instant::now();
-    let syntax = PS.find_syntax_by_extension("rs").unwrap();
-    let syntax_time = format!("Syntax load: {}", now.elapsed().as_millis());
-    let now = Instant::now();
-    let theme = &TS.themes["base16-ocean.dark"];
-    let theme_time = format!("Theme load: {}", now.elapsed().as_millis());
-    let s = include_str!("main.rs");
-    let now = Instant::now();
-    let html = highlighted_html_for_string(s, &PS, &syntax, theme);
-    let high_time = format!("Highlight time: {}", now.elapsed().as_millis());
-    html! {
-        <>
-        <h1>{ syntax_time }</h1>
-        <h1>{ theme_time }</h1>
-        <h1>{ high_time }</h1>
-        <RawHTML inner_html={ html } />
-        </>
+        let tmp = self.div.clone();
+        let html = Html::VRef(tmp.into());
+        html! {
+            <>
+            <h1>{ &self.time }</h1>
+            <textarea oninput={ctx.link().callback(|e: InputEvent| Msg::Update(e))} style="width: 100%; max-width: 100%;" rows="20"></textarea>
+            { html }
+            </>
+        }
     }
 }
 
